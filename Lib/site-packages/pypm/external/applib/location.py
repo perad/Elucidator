ca4c0ce4e98b8f9c3a769addbcf3df562288e078
booshitha <booshitha.hotmail.com>
2011-01-19 15:54:05 -0600

    Added python directory for some reason

---
+++ Lib/site-packages/pypm/external/applib/location.py
@@ -0,0 +1,12 @@
+# This module is deprecated
+
+from appdirs import *
+
+
+#---- self test code
+
+if __name__ == "__main__":
+    print("applib: user data dir: %s" % user_data_dir("Komodo", "ActiveState"))
+    print("applib: site data dir: %s" % site_data_dir("Komodo", "ActiveState"))
+    print("applib: user cache dir: %s" % user_cache_dir("Komodo", "ActiveState"))
+
